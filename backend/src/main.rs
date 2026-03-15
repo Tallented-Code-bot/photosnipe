1c334519e025c720224d1e327f2898f22f3deb2d
Calvin Tallent <calvintallentspam@gmail.com>
2026-03-14 21:37:05 -0700

    Set up basic web serving.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,27 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use rocket::fs::{FileServer, relative};
+use rocket::response::content::RawHtml;
+use std::path::{Path, PathBuf};
+use rocket::tokio::fs::File;
+use rocket::tokio::io::AsyncReadExt;
+use rocket::http::ContentType;
+
+#[get("/<_..>", rank = 20)]
+async fn spa_fallback() -> Option<(ContentType, String)> {
+    // Serve index.html for any GET request that doesn't match an API or static file
+    let index_path = Path::new(relative!("static")).join("index.html");
+    let mut file = File::open(index_path).await.ok()?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).await.ok()?;
+    Some((ContentType::HTML, contents))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/api", routes![]) // All API routes will be here in future
+        .mount("/", FileServer::from(relative!("static")).rank(0))
+        .mount("/", routes![spa_fallback])
 }
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +7,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: '../backend/static',
+			assets: '../backend/static',
+			fallback: 'index.html'
+		})
 	},
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) =>
